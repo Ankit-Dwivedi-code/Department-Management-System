94394a0379b8bfa33ef1594b9dff30452f45bd3f
Ankit-Dwivedi-code <152896806+Ankit-Dwivedi-code@users.noreply.github.com>
2024-07-26 16:59:14 +0530

    Admin controller, middleware and route

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1,7 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { Admin } from "../models/admin.model.js";
 import { InviteCode } from "../models/invite.model.js";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import { ApiError } from "../utils/apiError.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/apiResponse.js";
@@ -31,10 +31,12 @@ const generateAccessAndRefreshTokens = async (adminId) => {
 // Register Admin
 const registerAdmin = asyncHandler(async (req, res) => {
   // Get admin details
-  const { name, email, role,  password, department, phone } = req.body;
+  const { name, email, role, password, department, phone } = req.body;
 
   // Check for empty fields
-  if ([name, email, password, role, department, phone].some((field) => !field)) {
+  if (
+    [name, email, password, role, department, phone].some((field) => !field)
+  ) {
     throw new ApiError(400, "All fields are required");
   }
 
@@ -108,7 +110,9 @@ const loginAdmin = asyncHandler(async (req, res) => {
   }
 
   // Generate access and refresh tokens
-  const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(admin._id);
+  const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+    admin._id
+  );
   console.log(refreshToken);
   // Save the refresh token to the database
   admin.refreshToken = refreshToken;
@@ -116,8 +120,8 @@ const loginAdmin = asyncHandler(async (req, res) => {
 
   const options = {
     httpOnly: true,
-    secure: true
-}
+    secure: true,
+  };
 
   // Send response with tokens
   return res
@@ -134,31 +138,208 @@ const loginAdmin = asyncHandler(async (req, res) => {
 });
 
 const generateInviteCode = asyncHandler(async (req, res) => {
-    const { role } = req.body;
-  
-    // Validate role
-    const validRoles = ['teacher', 'student'];
-    if (!validRoles.includes(role)) {
-      throw new ApiError(400, 'Invalid role specified');
-    }
-  
-    // Create a unique invite code
-    const inviteCode = uuidv4();
-  
-    // Save the invite code to the database
-    const invite = new InviteCode({
-      code: inviteCode,
-      role,
-      used: false
-    });
-  
-    await invite.save();
-  
-    return res.status(201).json({
-      success: true,
-      message: 'Invite code generated successfully',
-      data: { inviteCode }
-    });
+  const { role } = req.body;
+
+  // Validate role
+  const validRoles = ["teacher", "student"];
+  if (!validRoles.includes(role)) {
+    throw new ApiError(400, "Invalid role specified");
+  }
+
+  // Create a unique invite code
+  const inviteCode = uuidv4();
+
+  // Save the invite code to the database
+  const invite = new InviteCode({
+    code: inviteCode,
+    role,
+    used: false,
   });
 
-export { registerAdmin, loginAdmin, generateInviteCode };
+  await invite.save();
+
+  return res.status(201).json({
+    success: true,
+    message: "Invite code generated successfully",
+    data: { inviteCode },
+  });
+});
+
+// Admin Logout
+const logoutAdmin = asyncHandler(async (req, res) => {
+  //clear the cookie
+  //clear rhe accesstoken
+  await Admin.findByIdAndUpdate(
+    req.admin._id,
+    {
+      $unset: {
+        refreshToken: 1,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "Admin Logged out successfully"));
+});
+
+const renewRefreshToken = asyncHandler(async(req, res)=>{
+  const token = req.cookies?.refreshToken || req.body.refreshToken
+
+  try {
+      const decodedToken = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET)
+  
+      if(!decodedToken){
+          throw new ApiError(401, "Unauthorized request")
+      }
+  
+      const admin = await Admin.findById(decodedToken._id)
+  
+      if(!admin){
+          throw new ApiError(401, "Inavild refresh Token")
+      }
+  
+  
+      if(token !== admin.refreshToken){
+          throw new ApiError(401, "Token doesnot match")
+      }
+  
+      const {refreshToken, accessToken} = generateAccessAndRefreshTokens(admin._id)
+  
+      const options = {
+          httpOnly : true,
+          secure : true
+      }
+  
+      return res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", refreshToken, options)
+      .json(
+          new ApiResponse(200, 
+              {
+                  accessToken,
+                  refreshToken
+              },
+              "Refresh token updated"
+          )
+      )
+  } catch (error) {
+      throw new ApiError(400, error?.message || "Invalid access token")
+  }
+})
+
+const changeCurrentPassword = asyncHandler(async(req, res)=>{
+
+  const {oldPassword, newPassword} = req.body
+  
+  if(!oldPassword || !newPassword){
+      throw new ApiError(400, "Please provide old and new password")
+  }
+
+  // Find the admin by ID
+  const admin = await Admin.findById(req.admin._id);
+  if (!admin) {
+    throw new ApiError(400, "Admin not found, please provide password correctly");
+  }
+
+   // Check if the old password is correct
+   const isPasswordCorrect = await admin.isPasswordCorrect(oldPassword);
+   if (!isPasswordCorrect) {
+     throw new ApiError(401, "Invalid old password");
+   }
+
+  admin.password = newPassword
+  await admin.save({validateBeforeSave : false })
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200, {}, "Password changed successfully"))
+})
+
+//to get the cureent admin
+const getCurrentAdmin = asyncHandler(async(req, res)=>{
+  // console.log(req.admin);
+  return res
+  .status(200)
+  .json(
+      new ApiResponse(200, req.admin, "Current user fetched successfully")
+  )
+})
+
+const updateAdminAvatar = asyncHandler(async(req, res)=>{
+
+  const avatarLocalPath = req.file.path;
+  console.log(avatarLocalPath);
+
+  if (!avatarLocalPath) {
+      throw new ApiError(400, "Please provide the avatar")
+  }
+
+  const avatar =await uploadOnCloudinary(avatarLocalPath)
+
+  if (!avatar.url) {
+      throw new ApiError(400, "Error while uploading on avatar")
+  }
+
+  const admin = await Admin.findByIdAndUpdate(req.admin?._id, 
+      {
+          $set:{
+              avatar : avatar.url
+          }
+      },
+      {new : true}
+  ).select("-password")
+
+  if(!admin){
+      throw new ApiError(400, "Admin not found")
+  }
+
+  return res
+  .status(200)
+  .json(
+      new ApiResponse(200, admin, "File uploaded successfully")
+  )
+})
+
+const updateAdminDetails = asyncHandler(async(req, res)=>{
+
+  const {name, email, phone} = req.body
+
+  if(!name || !email || !phone){
+      throw  new ApiError(400, "Please provide the details")
+  }
+
+  const admin = await Admin.findByIdAndUpdate(req.admin._id,
+      {
+          $set:{
+              name,
+              email,
+              phone
+          }
+      },
+      {new : true}
+  ).select("-password")
+
+  if(!admin){
+      throw new ApiError(400, "Admin not found")
+  }
+
+  return res
+  .status(200)
+  .json(
+      new ApiResponse(200, admin, "Admin details updated Successfully")
+  )
+})
+
+export { registerAdmin, loginAdmin, generateInviteCode, logoutAdmin, renewRefreshToken, changeCurrentPassword, getCurrentAdmin, updateAdminAvatar, updateAdminDetails };
--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -1,6 +1,7 @@
 import { Router } from "express"
 import { upload } from "../middleware/multer.middleware.js" 
-import { registerAdmin, loginAdmin, generateInviteCode } from "../controllers/admin.controller.js"
+import { registerAdmin, loginAdmin, generateInviteCode, logoutAdmin, renewRefreshToken, changeCurrentPassword, getCurrentAdmin, updateAdminAvatar, updateAdminDetails } from "../controllers/admin.controller.js"
+import { verifyAdmin } from "../middleware/auth.middleware.js"
 
 const router = Router()
 
@@ -17,6 +18,21 @@ router.route("/register").post(
 router.route("/log-in").post(loginAdmin)
 
 //Invite code route
-router.route("/generate-invite-code").post(generateInviteCode)
+router.route("/generate-invite-code").post(verifyAdmin, generateInviteCode)
+//logout admin route
+router.route("/log-out").post(verifyAdmin, logoutAdmin)
+//Renew refresh Token
+router.route("/renew-refresh-token").post(verifyAdmin, renewRefreshToken)
+//Change current password
+router.route("/change-password").post(verifyAdmin, changeCurrentPassword)
+//Get currtent admin
+router.route("/get-admin").get(verifyAdmin, getCurrentAdmin)
+//Update Admin avatar
+router.route("/update-avatar").patch(verifyAdmin,upload.single("avatar"), updateAdminAvatar)
+//Update admin details
+router.route("/update-details").patch(verifyAdmin, updateAdminDetails)
+
+
+
 
 export default router
